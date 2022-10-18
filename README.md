508b6b5d29261ea5a0748636d0052109c2d47d6f
Oikmo <genisis344@gmail.com>
2022-10-18 21:39:30 +0100

    model, view and projection matrices

--- README.md
+++ README.md
@@ -4,6 +4,5 @@ java game in opengl
 
 i need help
 
-right now : matrices and uniform variables
-
-next : model, view and projection matrices
+right now : model, view and projection matrices
+next : loading 3d models
--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,7 +1,9 @@
 package engineTester;
 
 import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
 
+import entities.*;
 import models.*;
 import textures.*;
 import renderEngine.*;
@@ -13,39 +15,105 @@ public class MainGameLoop {
 		
 		DisplayManager.createDisplay();
 		Loader loader = new Loader();
-		Renderer renderer = new Renderer();
 		StaticShader shader = new StaticShader();
+		Renderer renderer = new Renderer(shader);
 		
-		float[] vertices = {
-				-0.5f, 0.5f, 0f,//v0
-				-0.5f, -0.5f, 0f,//v1
-				0.5f, -0.5f, 0f,//v2
-				0.5f, 0.5f, 0f,//v3
-		};
 		
-		int[] indices = {
-				0,1,3,//top left triangle (v0, v1, v3)
-				3,1,2//bottom right triangle (v3, v1, v2)
+		float[] vertices = {			
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,0.5f,-0.5f,		
+				
+				-0.5f,0.5f,0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				0.5f,0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				-0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,0.5f,
+				-0.5f,0.5f,-0.5f,
+				0.5f,0.5f,-0.5f,
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,-0.5f,0.5f,
+				-0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,0.5f
+				
 		};
 		
 		float[] textureCoords = {
-			0,0, //v0
-			0,1, //v1
-			1,1, //v2
-			1,0  //v3
+				
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0
+
+				
+		};
+		
+		int[] indices = {
+				0,1,3,	
+				3,1,2,	
+				4,5,7,
+				7,5,6,
+				8,9,11,
+				11,9,10,
+				12,13,15,
+				15,13,14,	
+				16,17,19,
+				19,17,18,
+				20,21,23,
+				23,21,22
+
 		};
 		
 		
 		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
 		ModelTexture texture = new ModelTexture(loader.loadTexture("textures/minos"));
-		TexturedModel texturedModel = new TexturedModel(model, texture);
+		TexturedModel staticModel = new TexturedModel(model, texture);
+		
+		Entity entity = new Entity(staticModel, new Vector3f(0,0,-5),0,0,0,1);
+		
+		Camera camera = new Camera();
 
 		while(!Display.isCloseRequested()) {
+			entity.increaseRotation(1, 1, 0);
+			camera.move();
 			renderer.prepare();
-			//   [game logic here]
 			shader.start();
-			renderer.render(texturedModel);
-			//if you dont stop it, it gives this trippy lsd effect
+			shader.loadViewMatrix(camera);
+			renderer.render(entity, shader);
 			shader.stop();
 			DisplayManager.updateDisplay();
 		}
